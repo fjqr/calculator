318e129e46f1c1239e086809d8cfee55231ca284
Francisco Quintero <franperc@gmail.com>
2020-03-20 01:24:00 -0400

    express

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const handleServer = function(req, res){
+    res.writeHead(200,{'content-type':'text/html'})
+res.write('<h1>Hola Mundo</h1>')
+res.end()
+    
+} 
+
+
+const server = http.createServer(handleServer)
+
+server.listen(3000, function(){
+    console.log('Server on port 3000')
+})
