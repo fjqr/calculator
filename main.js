24255dc07ba98da88c783ae6c37c3a907973538a
Francisco Quintero <franperc@gmail.com>
2020-03-23 21:37:39 -0400

    depurado general

--- main.js
+++
@@ -1,6 +0,0 @@
-const add = require('./add.js')
-
-const screen = document.querySelector('.screen') //pantalla de la calculadora
-const buttons = [...document.querySelectorAll('.buttons > div')] //botones
-
-console.log(add.add(45,56))
