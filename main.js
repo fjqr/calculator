216658382d9e3bae130a145d2c91d64d926b158c
Francisco <franperc@gmail.com>
2021-05-29 19:01:30 -0400

    Solo muestra el resultado final

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
-let buttons = [...document.querySelectorAll('.buttons')]
-let equal = document.querySelector('.equal')
-let add = document.querySelector('.add')
-let screen = document.querySelector('.screen')
-let clear = document.querySelector('.clear')
+let buttons = [...document.querySelectorAll(".buttons")];
+let equal = document.querySelector(".equal");
+let add = document.querySelector(".add");
+let screen = document.querySelector(".screen");
+let clear = document.querySelector(".clear");
 
 getButtons = () => {
-buttons.forEach(v =>{
-
-     v.addEventListener('click',(e) =>{
-        let value =  document.createTextNode(e.target.value)
-        e.target === equal ? value = document.createTextNode(operations(screen.innerText)) : ''
-        screen.appendChild(value) 
-    })
+  buttons.forEach((v) => {
+    v.addEventListener("click", (e) => {
         
-})
-clear.addEventListener('click' ,() =>{ screen.innerText = ''})
+        let value = document.createTextNode(e.target.value);
+        screen.appendChild(value);
 
-}
+    if(e.target ===  equal){
+       operations(screen.innerText)
+    }
+    
+    });
+  });
+  clear.addEventListener("click", () => {
+    screen.innerText = "";
+  });
+};
 
+operations = (a) => {
+  let numA = parseInt(a.match(/^[0-9]+/g).join());
+  let numB = parseInt(a.match(/[0-9]+$/g).join());
+  let add = a.match(/\+{1}/g);
+  let subs = a.match(/\-{1}/g);
+  let mult = a.match(/\*{1}/g);
+  let div = a.match(/\/{1}/g);
 
-operations = a => {
-    
-    let numA = parseInt(a.match(/^[0-9]+/g).join())
-    let numB = parseInt(a.match(/[0-9]+$/g).join())
-    let add = a.match(/\+{1}/g)
-    let subs = a.match(/\-{1}/g)
-    let mult = a.match(/\*{1}/g)
-    let div = a.match(/\/{1}/g)
-    
-    
-    return add ?  ` = ${numA + numB}`: subs ? ` = ${numA - numB}` : mult ? ` = ${numA * numB}` : div && numB > 0 ? ` = ${numA / numB}` : ''
-    
-}
+  return add
+    ? ` = ${screen.innerText = numA + numB}`
+    : subs
+    ? ` = ${screen.innerText = numA - numB}`
+    : mult
+    ? ` = ${screen.innerText = numA * numB}`
+    : div && numB > 0
+    ? ` = ${screen.innerText = numA / numB}`
+    : "";
+};
 
-    getButtons()
+getButtons();
